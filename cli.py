dd9f3f8b26d8c94796923aabd2055c6e0c0cddd2
Samuel Chu <samuelechu@umail.ucsb.edu>
2017-05-19 14:55:34 -0700

    update cli

--- cli.py
+++ cli.py
@@ -5,20 +5,30 @@ import time
 import threading
 import sys
 
+
+cli = None
+
+
 def main():
+	global cli
 	if(len(sys.argv) != 3):
 		print("USAGE: python [machine_id] [setup_file]")
 		exit(1)
 	machine_id = int(sys.argv[1])
-	machine = Machine(machine_id)
+	cli = Machine(machine_id)
 	setup_file = sys.argv[2]
-	# setup(machine, setup_file)
+	setup(cli, setup_file)
+
+	cThread = threading.Thread(target = commThread)
+	cThread.daemon = True
+	cThread.start()
 
 	while True:
 		command = raw_input()
 
 		if command == "replicate":
-			print command
+			cli.outgoingSocket.send("replicate!")
+
 
 		elif command == "map":
 			print "map"
@@ -28,7 +38,29 @@ def main():
 
 		elif command == "resume":
 			print "resume"	
+
+		elif command == "exit":
+			cli.outgoingSocket.send("exit")
+			break
 	# execute_commands(site, command_file)
+
+def commThread():
+
+	while True:
+		try:
+			con, _ = cli.listeningSocket.accept()
+			con.setblocking(0)
+			cli.incomingStream = con
+			break
+		except socket.error:
+			continue
+
+	while True:
+		try:
+			data = cli.incomingStream.recv(1024)
+			print data		
+		except socket.error, e:
+			continue
 
 def setup(machine, setup_file):
 	#Read setup file. ex - setup.txt     
@@ -38,23 +70,50 @@ def setup(machine, setup_file):
 		process_id = 0
 		for line in f.readlines():
 			process_id += 1
-			if process_id <= N:
-				# 1 -> cli 2-> prm
+			if process_id == machine.id :
 				IP1, port1, IP2, port2 = line.strip().split()
 				port1 = int(port1)
 				port2 = int(port2)
 				print "{0} {1}".format(port1, port2)
+				machine.cli = (IP1, port1)
+				machine.prm = (IP2, port2)
 
-			 	machine.addr_book_cli.append( (IP1, port1) )
-			 	machine.addr_book_prm.append( (IP2, port2) )
-			 	machine.addOutgoingConn()
 
-			 	if process_id != machine_id:
-			 		machine.addOutgoingConn(process_id)
-			 		# machine.addIncomingConn(process_id)
+				machine.openListeningSocket(IP1, port1)
+				print "got here!"
+				# connect to prm
+				while True:
+					try: 
+						sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+						sock.connect(machine.prm)
+						machine.outgoingSocket = sock
+						break
+					except Exception:
+						continue
+				print "got here!"
 
-				else:
-					machine.openListeningSocket( IP1, port1 ) #open for traffic
+				
+
+
+
+
+			# if process_id <= N:
+			# 	# 1 -> cli 2-> prm
+			# 	IP1, port1, IP2, port2 = line.strip().split()
+			# 	port1 = int(port1)
+			# 	port2 = int(port2)
+			# 	print "{0} {1}".format(port1, port2)
+
+			#  	machine.addr_book_cli.append( (IP1, port1) )
+			#  	machine.addr_book_prm.append( (IP2, port2) )
+			#  	machine.addOutgoingConn()
+
+			#  	if process_id != machine_id:
+			#  		machine.addOutgoingConn(process_id)
+			#  		# machine.addIncomingConn(process_id)
+
+			# 	else:
+			# 		machine.openListeningSocket( IP1, port1 ) #open for traffic
 			# else:
 			# 	source_id, dest_id = line.strip().split()
 			# 	source_id = int(source_id)
@@ -63,7 +122,7 @@ def setup(machine, setup_file):
 			# 		site.addOutgoingChannel(dest_id)
 			# 	if dest_id == site.id: #I am the receive
 			# 		site.addIncomingChannel(source_id)
-	machine.openOutgoingConn()
+	# machine.openOutgoingConn()
 	# machine.openIncomingConn()
 
 
@@ -71,8 +130,11 @@ class Machine(object):
 	def __init__(self, machine_id):
 		self.id = machine_id
 		self.num_proc = 0
-		self.addr_book_cli = []
-		self.addr_book_prm = []
+		self.cli = ()
+		self.prm = ()
+
+		self.outgoingSocket = None
+		self.incomingStream = None
 		self.outgoing_conn = {}
 		self.listeningSocket = None
 
@@ -82,41 +144,16 @@ class Machine(object):
 		self.listeningSocket.setblocking(0) 
 		self.listeningSocket.listen(10)
 
-	def addOutgoingConn(self, dest_id):
-		cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		prm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.outgoing_conn[dest_id] = [cli, prm]
 
-
-	def openOutgoingConn(self):
-
-		for dest_id, sock in self.outgoing_conn.iteritems():
-			if dest_id != self.id:
-				# connect to cli
-				while True:
-					try: 
-						sock[0].connect(self.addr_book_cli[dest_id - 1])
-						break
-					except Exception:
-						continue
-				#connect to prm
-				while True:
-					try: 
-						sock[1].connect(self.addr_book_prm[dest_id - 1])
-						break
-					except Exception:
-						continue
-
-	def openIncomingConn(self):
-		print " "
-		# while len(self.incoming_channels) != len(self.SnapIDTableLastEntryTemplate):
-		# 	try:
-		# 		con, _ = self.listeningSocket.accept()
-		# 		con.setblocking(0)
-		# 		self.incoming_channels.append(con)
-		# 	except socket.error:
-		# 		continue
-
+	def acceptPRM(self):
+		while True:
+			try:
+				con, _ = self.listeningSocket.accept()
+				con.setblocking(0)
+				self.incomingStream = con
+				break
+			except socket.error:
+				continue
 
 
 
--- prm.py
+++ prm.py
@@ -1,99 +1,176 @@
-#!/usr/bin/env python
-
 import socket
 import time
 import threading
 import sys
+import Queue
+import copy
 
 def main():
 	if(len(sys.argv) != 3):
-		print("USAGE: python [machine_id] [setup_file]")
+		print("USAGE: python [prm_id] [setup_file]")
 		exit(1)
-	machine_id = int(sys.argv[1])
-	machine = Machine(machine_id)
+	site_id = int(sys.argv[1])
+	site = Site(site_id)
 	setup_file = sys.argv[2]
-	# setup(machine, setup_file)
+	setup(site, setup_file)
+	print "success"
+	commThread(site)
 
-	# execute_commands(site, command_file)
 
-def setup(machine, setup_file):
-	#Read setup file. ex - setup.txt     
+def commThread(site):
+
+	finish = 0
+	while finish == 0:
+
+		for con in site.incoming_channels:	
+			try:
+				data = con.recv(1024)
+				print data	
+				if data == "replicate!":
+					for dest_id, sock in site.outgoing_channels.iteritems():
+						sock.send("replicate from node {0}".format(dest_id))
+					
+				elif data == "exit"	:
+					for dest_id, sock in site.outgoing_channels.iteritems():
+						sock.send("exit")
+					finish = 1
+			except socket.error, e:
+				continue
+
+def setup(site, setup_file):
+	#Read setup file. ex - setup.txt  
+	print "gothere"   
 	with open(setup_file, 'r') as f:
 		N = int(f.readline().strip())
-		machine.num_proc = N
+		site.num_proc = N
 		process_id = 0
 		for line in f.readlines():
 			process_id += 1
 			if process_id <= N:
-				# 1 -> cli 2-> prm
 				IP1, port1, IP2, port2 = line.strip().split()
 				port1 = int(port1)
 				port2 = int(port2)
-				print "{0} {1}".format(port1, port2)
 
-			 	machine.addr_book_cli.append( (IP1, port1) )
-			 	machine.addr_book_prm.append( (IP2, port2) )
+				if process_id == site.id:
+					site.addr_book.append( (IP1, port1) )
+					site.openListeningSocket( IP2, port2) #open for traffic
 
-				if process_id == machine.id:
-					machine.openListeningSocket( IP1, port1 ) #open for traffic
-			# else:
-			# 	source_id, dest_id = line.strip().split()
-			# 	source_id = int(source_id)
-			# 	dest_id = int(dest_id)
-			# 	if source_id == site.id: #I am the sender
-			# 		site.addOutgoingChannel(dest_id)
-			# 	if dest_id == site.id: #I am the receive
-			# 		site.addIncomingChannel(source_id)
-	machine.openOutgoingConn()
-	machine.openIncomingConn()
+				else:
+					site.addr_book.append( (IP2, port2) )
+
+				site.addOutgoingChannel(process_id)
+				site.addIncomingChannel(process_id)
+
+	print site.outgoing_channels
+	print site.incoming_channels
+	site.openOutgoingChannels()
+	print "am"
+	site.openIncomingChannels()
 
 
-class Machine(object):
-	def __init__(self, machine_id):
-		self.id = machine_id
-		self.num_proc = 0
-		self.addr_book_cli = []
-		self.addr_book_prm = []
+
+class Site(object):
+	def __init__(self, site_id):
+		self.id = site_id
+		self.snap_count = 0
+		self.snap_count = 0
+		self.balance = 10
+		self.incoming_channels = []
+		self.SnapIDTableLastEntryTemplate = {}
+		self.addr_book = []
 		self.outgoing_channels = {}
 		self.listeningSocket = None
+		self.snapID_table = {}
+		self.done_processes = set()
 
 	def openListeningSocket(self, IP, port):
 		self.listeningSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.listeningSocket.bind( (IP, port) )
 		self.listeningSocket.setblocking(0) 
-		self.listeningSocket.listen(10)
+		self.listeningSocket.listen(1)
 
-	def openOutgoingConn(self):
-		for dest_id in range(1, num_proc):
-			if dest_id != self.id:
-				while True:
-					try: 
-						sock.connect(self.addr_book_cli[dest_id - 1])
-						break
-					except Exception:
-						continue
+	def addOutgoingChannel(self, dest_id):
+		self.outgoing_channels[dest_id] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-				while True:
-					try: 
-						sock.connect(self.addr_book_prm[dest_id - 1])
-						break
-					except Exception:
-						continue
+	def openOutgoingChannels(self):
+		#time.sleep(5) #To make sure all other processes are up
+		for dest_id, sock in self.outgoing_channels.iteritems():
+			while True:
+				try: 
+					sock.connect(self.addr_book[dest_id - 1])
+					break
+				except Exception:
+					continue
+
+	def addIncomingChannel(self, source_id):
+		self.SnapIDTableLastEntryTemplate[source_id] = [False, 0]
 
 	def openIncomingChannels(self):
-		print "hey"
-		# while len(self.incoming_channels) != len(self.SnapIDTableLastEntryTemplate):
-		# 	try:
-		# 		con, _ = self.listeningSocket.accept()
-		# 		con.setblocking(0)
-		# 		self.incoming_channels.append(con)
-		# 	except socket.error:
-		# 		continue
+		while len(self.incoming_channels) != len(self.SnapIDTableLastEntryTemplate):
+			try:
+				con, _ = self.listeningSocket.accept()
+				con.setblocking(0)
+				self.incoming_channels.append(con)
+			except socket.error:
+				continue
 
+	def execute(self, command):
+		self.checkIncomingMsgs()
+		keyWords = command.split()
+		if "send" == keyWords[0]:
+			dest_id = int(keyWords[1])
+			amount = int(keyWords[2])
+			self.sendMoney(dest_id, amount)
+		elif "snapshot" == keyWords[0]:
+			self.startSnapshot()
+		elif "sleep" == keyWords[0]:
+			time = float(keyWords[1])
+			self.sleep(time)
+		else:
+			print "CANNOT RECOGNIZE THE COMMAND: " + command
+			exit(1)
+		self.checkIncomingMsgs()
 
-
-
-
+	def checkIncomingMsgs(self):
+		BUF_SIZE = 1024
+		for con in self.incoming_channels:
+			try:
+				msgs = con.recv(BUF_SIZE)
+				for msg in Message.split(msgs):
+					msg = Message.reconstructFromString(msg.strip())
+					if msg.type == Message.MARKER_TYPE:
+						if msg.snap_id not in self.snapID_table: #First Marker -> input into snapID_table
+							counter = 1
+							site_state = self.balance #Take Local State Snapshot
+							incoming_channels_states = copy.deepcopy(self.SnapIDTableLastEntryTemplate)
+							self.snapID_table[msg.snap_id] = [counter, site_state, incoming_channels_states]
+							self.snapID_table[msg.snap_id][2][msg.source_id][0] = True #Shut down the incoming channel of 1st Marker
+							self.sendMarkers(msg.snap_id)
+							if self.snapID_table[msg.snap_id][0] == len(self.incoming_channels): #Received all markers
+								self.outputLocalSnapshotAt(msg.snap_id)
+						else: #Not the first marker
+							self.snapID_table[msg.snap_id][0] += 1 #increase counter by 1
+							self.snapID_table[msg.snap_id][2][msg.source_id][0] = True #Take no more Snapshot on the channel
+							if self.snapID_table[msg.snap_id][0] == len(self.incoming_channels): #Received all markers
+								self.outputLocalSnapshotAt(msg.snap_id)
+					elif msg.type == Message.MONEY_TRANSFER_TYPE: #received a msg
+						self.balance += msg.amount #Fix the real-time balance
+						for _, v in self.snapID_table.iteritems():
+							if v[0] == len(self.incoming_channels): #Finished Snapshot
+								continue
+							if v[2][msg.source_id][0] == False: #The current money message is before Marker
+								v[2][msg.source_id][1] += msg.amount #Record the increase in amount
+					elif msg.type == Message.DONE_TYPE:
+						#Piggy back ammount as done_process_id
+						done_process_id = msg.amount
+						if done_process_id not in self.done_processes:
+							self.done_processes.add(done_process_id)
+							self.sendDone(done_process_id)
+					else:
+						print "ERROR: DO NOT UNDERSTAND MESSAGE TYPE"
+						print msg
+						exit(1)
+			except socket.error, e:
+				continue
 
 main()
-
