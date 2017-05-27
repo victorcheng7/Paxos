45192ad6fd32967ddd5474c60cda5a26872bd1a3
Samuel Chu <samuelechu@umail.ucsb.edu>
2017-05-26 23:53:19 -0700

    bug free hopefully

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@ killall -9 python
 
 for n in {1..3}
 do
-gnome-terminal --tab set-title "Prm $n" -e "./prm $n ./Setup/setup2.txt"
-gnome-terminal --tab set-title "Cli $n" -e "./cli $n ./Setup/setup2.txt"
+gnome-terminal --tab set-title "Prm $n" -e "python prm.py $n ./Setup/setup2.txt"
+gnome-terminal --tab set-title "Cli $n" -e "python cli.py $n ./Setup/setup2.txt"
 done
 
 
